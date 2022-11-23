2a113b4d459468ea8e531496f8ebf0529c722973
Kyle Baumgarten <kcbaumga315@gmail.com>
2022-11-23 16:12:35 -0500

    sql

--- restclient/migrations/2022-11-23-210232_create_posts/up.sql
+++ restclient/migrations/2022-11-23-210232_create_posts/up.sql
@@ -1,1 +1,6 @@
--- Your SQL goes here
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  body TEXT NOT NULL,
+  published BOOLEAN NOT NULL DEFAULT FALSE
+);-- Your SQL goes here
